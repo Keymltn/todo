6a432132ba937a086d53c12536f413d9ccc6e74c
Keymltn <gulnozarahmatova21@gmail.com>
2022-09-14 14:40:41 +0500

    la

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ let input = document.querySelector(".input")
 let del = document.querySelector(".delete")
 let app = document.querySelector(".app")
 let deleteBox = document.querySelector(".box4")
+let yes = document.querySelector(".yes")
 console.log(deleteBox);
 
 del.addEventListener("click" , () => {
     app.classList.toggle('none')
-    deleteBox.classList.toggle('flex-wr')
+    deleteBox.classList.add('flex-wr')
+})
+
+del.addEventListener("click" , () => {
+    yes.classList.add('yes')
 })
